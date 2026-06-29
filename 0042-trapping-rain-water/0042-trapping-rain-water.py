5bda3c9fa616b5310c5e77c96f928690e9b33fd9
nehakanwadiya <kanwadiyaneha4@gmail.com>
2026-06-29 15:54:46 +0530

    Time: 11 ms (45.32%), Space: 21.2 MB (23.95%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,25 +1,50 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+
+# Auxillary Array and prefix array approach   
+# Time = O(n), Space = O(n)     
+        # n = len(height)
+        # left =  [0]*n
+        # right = [0]*n
+        # ans = 0
+
+        # left[0] = height[0]
+        # for i in range(1, n):
+        #     left[i] = max(left[i-1], height[i])
+
+        # right[n-1] = height[n-1]    
+        # for i in range(n-2, -1, -1):
+        #     right[i] = max(right[i+1], height[i])
         
+        # for i in range(n):
+        #     ans += min(left[i], right[i])-height[i]
+
+        # return ans
+
+
+#2 pointers approach
+# time = O(n), space = O(1)
+
         n = len(height)
-        left =  [0]*n
-        right = [0]*n
+        l = 0
+        r = n-1
         ans = 0
+        lmax = height[0]
+        rmax = height[n-1]
 
-        left[0] = height[0]
-        for i in range(1, n):
-            left[i] = max(left[i-1], height[i])
+        while l < r:
+            lmax = max(lmax, height[l])
+            rmax = max(rmax, height[r])
 
-        right[n-1] = height[n-1]    
-        for i in range(n-2, -1, -1):
-            right[i] = max(right[i+1], height[i])
-        
-        for i in range(n):
-            ans += min(left[i], right[i])-height[i]
+            if lmax < rmax:
+                ans += lmax - height[l]
+                l = l+1
+
+            else:
+                ans += rmax - height[r]
+                r = r-1
 
         return ans
-
-
 
 
         
