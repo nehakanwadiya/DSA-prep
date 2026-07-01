a2f3f1b0e87ffa8e75b5a91b216b2bdf1e749e58
nehakanwadiya <kanwadiyaneha4@gmail.com>
2026-07-01 14:10:59 +0530

    Time: 66 ms (20.14%), Space: 19.4 MB (91.16%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
@@ -1,7 +1,7 @@
 import math
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
-        while k:
+        while k>0:
             largest = max(gifts)
             root = math.isqrt(largest)
             max_index = gifts.index(largest)
