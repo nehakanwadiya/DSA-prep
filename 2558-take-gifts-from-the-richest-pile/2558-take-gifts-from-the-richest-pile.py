c18eed35c0d4444d8bedc359a673332579209a86
nehakanwadiya <kanwadiyaneha4@gmail.com>
2026-07-01 13:06:28 +0530

    Time: 71 ms (17.54%), Space: 19.3 MB (97.68%) - LeetHub

---
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
@@ -0,0 +1,12 @@
+import math
+class Solution:
+    def pickGifts(self, gifts: List[int], k: int) -> int:
+        while k:
+            largest = max(gifts)
+            root = math.isqrt(largest)
+            max_index = gifts.index(largest)
+            gifts[max_index] = root
+            k -=1
+
+        return sum(gifts)
+        
